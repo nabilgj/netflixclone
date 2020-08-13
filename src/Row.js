4d85f3467426a3f097d4d2ff96ad93f948decefc
nailgj.bnm@gmail.com <nabilgj.bnm@gmail.com>
2020-08-13 17:51:40 -0400

    added Row component with all the api calls to the end point coming from App component through props using async await function, setting our movies state, mapping through the arrays of data we got back from api and show them in our Row component. Made some beautiful styling in row css file

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import "./Row.css";
 
 import axios from "./axios";
 
-const Row = ({ title, fetchUrl }) => {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition
@@ -11,19 +14,34 @@ const Row = ({ title, fetchUrl }) => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
 
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
 
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  console.table(movies);
 
   return (
-    <div>
+    <div className="row">
       {/* title */}
       <h2>{title}</h2>
 
       {/* container ==> posters */}
+      <div className="rows__posters">
+        {/* several row posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
