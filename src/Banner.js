5f551ec685376e142b023954b66909e3eac42f5e
nailgj.bnm@gmail.com <nabilgj.bnm@gmail.com>
2020-08-14 08:59:58 -0400

    added Banner component with its title button description and put background image to the header element which contains all other elements. Added some styling and gave some linear gradient at the bottom to give some subtle overlay effect

---
+++ src/Banner.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import "./Banner.css";
+
+import axios from "./axios";
+import requests from "./requests";
+
+const Banner = (props) => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* title */}
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        {/* div containing 2 buttons */}
+        <div className="banner__buttons">
+          <button className="banner__button"> Play </button>
+          <button className="banner__button"> My List </button>
+        </div>
+
+        {/* description */}
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+};
+
+// go into App
+export default Banner;
--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,8 @@
 const API_KEY = "2d90d25672e18e28ea73dda6adcb3087";
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
